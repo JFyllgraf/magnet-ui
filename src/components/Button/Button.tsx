40feec00a316d8454d41601966c6f674b249ee7d
Jesper <jesper92fyllgraf@hotmail.com>
2023-12-16 18:42:57 +0100

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+    children: React.ReactNode;
+}
+const Button = (props: Props) => {
+    const { children } = props;
+
+    return (
+        <button>{children}</button>
+    )
+}
+
+export default Button;
